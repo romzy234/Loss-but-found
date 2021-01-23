271b39629a8c6ec684fe480620e765768f048b1c
Ogoh Cyril <cyrilogoh@gmail.com>
2021-01-23 12:38:32 +0100

    fixed auth

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const { config } = require('./config.config');
 var createError = require('http-errors');
 var passport = require('passport');
 const multer = require('multer');
+var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+var session = require('express-session');
+
 
 // hashing ---Note
 //sql -- later
 // createError 
-// express Session ---note
+
 
 
 // Express 
@@ -26,6 +30,21 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: "ilovecoding",//process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,  
+  cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+})); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//passport config
+require('./auth/passport');
+
 const port = process.env.PORT || '3000';
 app.listen(port, ()=>{
     console.log(`running on port ${port} or go to 'http://localhost:3000' `)
@@ -36,8 +55,7 @@ app.listen(port, ()=>{
 //     next(createError(404));
 //   });
 
-//passport config
-require('./auth/passport');
+
 
 app.use((req, res, next) => {
     res.locals.logUser = req.user;  
--- router/users.js
+++ router/users.js
@@ -2,15 +2,22 @@ var express = require('express');
 var router = express.Router();
 var usercontroller = require('../controller/user');
 const path = require('path');
+const passport = require('passport');
 
 router.use(express.static(path.join(__dirname, 'uploads')));
 
 router.get('/signup',usercontroller.getsignup);
 router.post('/signup',usercontroller.postsignup);
 router.get('/signin',usercontroller.getsignin);
-router.post('/signin',usercontroller.postsignin);
+// router.post('/signin',usercontroller.postsignin);
 router.get('/test2',usercontroller.getTest);
 router.post('/upload-multiple-images', usercontroller.postTest);
-//router.get('/register',check,usercontroller.getRegister);
+
+router.post('/signin',passport.authenticate('local', { failureRedirect: '/', successRedirect: '/users/homepage' }));
+
+router.get('/logout', function(req, res, next) {
+    req.logout();
+    res.redirect('/users');
+  });
 
 module.exports = router;
