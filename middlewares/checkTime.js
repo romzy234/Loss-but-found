c66c3866a0f0e1dfb87f6f5e626342add0b9f0fd
Ogoh Cyril <cyrilogoh@gmail.com>
2021-02-04 20:00:45 +0100

    Set Up final

--- middlewares/checkTime.js
+++ middlewares/checkTime.js
@@ -3,9 +3,9 @@ exports.time = (req, res, next) => {
      const now = Date.now();
      if (now - time < 300000){
          next();
-         console.log(now - time + ' Not expired')
+        //  console.log(now - time + ' Not expired')
      }else{
-        console.log(now - time + ' expired')
+        // console.log(now - time + ' expired')
          return res.send('Expired link')
          // write condition
      }
